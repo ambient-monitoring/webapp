60e342b4c0952ea6a1487026690340e51d5a3bc6
Emil Burzo <eaglex@gmail.com>
2015-10-27 11:54:46 +0200

    temperature gauge; indoor flag

--- src/main/java/org/ambientmonitoring/webapp/client/chart/TemperatureGaugeChart.java
+++ src/main/java/org/ambientmonitoring/webapp/client/chart/TemperatureGaugeChart.java
@@ -12,7 +12,7 @@ import org.moxieapps.gwt.highcharts.client.plotOptions.Dial;
 import org.moxieapps.gwt.highcharts.client.plotOptions.GaugePlotOptions;
 import org.moxieapps.gwt.highcharts.client.plotOptions.Pivot;
 
-public class HumidityGaugeChart extends FlowPanel {
+public class TemperatureGaugeChart extends FlowPanel {
 
     private final Integer sensorId;
     private final String title;
@@ -20,7 +20,7 @@ public class HumidityGaugeChart extends FlowPanel {
 
     private long lastTimestamp;
 
-    public HumidityGaugeChart(Integer sensorId, String title, boolean indoor) {
+    public TemperatureGaugeChart(Integer sensorId, String title, boolean indoor) {
         this.sensorId = sensorId;
         this.title = title;
         this.indoor = indoor;
@@ -33,14 +33,14 @@ public class HumidityGaugeChart extends FlowPanel {
 
             @Override
             public void onSuccess(ReadingRPC result) {
-                add(createChart(result.humidity));
+                add(createChart(result.temperature));
 
                 lastTimestamp = result.timestamp;
             }
         });
     }
 
-    public Chart createChart(double humidity) {
+    public Chart createChart(double temperature) {
         setWidth("400px");
 
         final Chart chart = new Chart()
@@ -52,13 +52,14 @@ public class HumidityGaugeChart extends FlowPanel {
                 .setBackgroundColor("#ffffff")
                 .setCredits(new Credits().setEnabled(false))
                 .setChartTitle(new ChartTitle().setText(title))
-                .setPane(new Pane().setStartAngle(-160).setEndAngle(160));
+                .setPane(new Pane().setStartAngle(-150).setEndAngle(150)
+                );
 
         chart.setGaugePlotOptions(new GaugePlotOptions()
                         .setDataLabels(new DataLabels()
                                 .setEnabled(true)
                                 .setY(30)
-                                .setFormat("{point.y}%"))
+                                .setFormat("{point.y} °C"))
                         .setPivotOptions(new Pivot().setRadius("3%"))
                         .setDialOptions(new Dial()
                                         .setRearLength("20%")
@@ -70,8 +71,8 @@ public class HumidityGaugeChart extends FlowPanel {
 
         // Primary axis
         chart.getYAxis(0)
-                .setMin(0)
-                .setMax(100)
+                .setMin(-20)
+                .setMax(50)
                 .setTickPosition(Axis.TickPosition.INSIDE)
                 .setMinorTickPosition(Axis.TickPosition.INSIDE)
                 .setLineColor("#888")
@@ -93,34 +94,34 @@ public class HumidityGaugeChart extends FlowPanel {
         // #DF5353
 
         if (indoor) {
-            YAxis yAxis = chart.getYAxis();
-            yAxis.setPlotBands(yAxis.createPlotBand()
-                            .setColor("#DF5353")
-                            .setFrom(0)
-                            .setTo(20),
-                    yAxis.createPlotBand()
-                            .setColor("#DDDF0D")
-                            .setFrom(20)
-                            .setTo(40),
-                    yAxis.createPlotBand()
-                            .setColor("#55BF3B")
-                            .setFrom(40)
-                            .setTo(60),
-                    yAxis.createPlotBand()
-                            .setColor("#DDDF0D")
-                            .setFrom(60)
-                            .setTo(70),
-                    yAxis.createPlotBand()
-                            .setColor("#DF5353")
-                            .setFrom(70)
-                            .setTo(100)
-            );
+//        YAxis yAxis = chart.getYAxis();
+//        yAxis.setPlotBands(yAxis.createPlotBand()
+//                        .setColor("#DF5353")
+//                        .setFrom(0)
+//                        .setTo(20),
+//                yAxis.createPlotBand()
+//                        .setColor("#DDDF0D")
+//                        .setFrom(20)
+//                        .setTo(40),
+//                yAxis.createPlotBand()
+//                        .setColor("#55BF3B")
+//                        .setFrom(40)
+//                        .setTo(60),
+//                yAxis.createPlotBand()
+//                        .setColor("#DDDF0D")
+//                        .setFrom(60)
+//                        .setTo(70),
+//                yAxis.createPlotBand()
+//                        .setColor("#DF5353")
+//                        .setFrom(70)
+//                        .setTo(100)
+//        );
         }
 
         final Series series = chart.createSeries();
         chart.addSeries(series
-                        .setName("Humidity %")
-                        .addPoint(humidity)
+                        .setName("Temperature °C")
+                        .addPoint(temperature)
         );
 
         Timer tempTimer = new Timer() {
@@ -144,7 +145,7 @@ public class HumidityGaugeChart extends FlowPanel {
             @Override
             public void onSuccess(ReadingRPC result) {
                 if (result.timestamp > lastTimestamp) {
-                    series.getPoints()[0].update(result.humidity);
+                    series.getPoints()[0].update(result.temperature);
 
                     lastTimestamp = result.timestamp;
                 }
--- src/main/java/org/ambientmonitoring/webapp/client/layout/Layout.java
+++ src/main/java/org/ambientmonitoring/webapp/client/layout/Layout.java
@@ -3,20 +3,34 @@ package org.ambientmonitoring.webapp.client.layout;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import org.ambientmonitoring.webapp.client.chart.HumidityGaugeChart;
+import org.ambientmonitoring.webapp.client.chart.TemperatureGaugeChart;
 
 public class Layout extends FlowPanel {
 
     public Layout() {
+        HorizontalPanel hp2 = new HorizontalPanel();
+        add(hp2);
+
+        hp2.add(new TemperatureGaugeChart(2, "Living Room - Temperature", true));
+        hp2.add(new TemperatureGaugeChart(1, "Bedroom 1 - Temperature", true));
+        hp2.add(new TemperatureGaugeChart(3, "Kitchen - Temperature", true));
+        hp2.add(new TemperatureGaugeChart(4, "Outside (NW) - Temperature", false));
+        
+        //////////
+
         HorizontalPanel hp = new HorizontalPanel();
         add(hp);
 
-        hp.add(new HumidityGaugeChart(2, "Living Room - Humidity"));
-        hp.add(new HumidityGaugeChart(1, "Bedroom 1 - Humidity"));
-        hp.add(new HumidityGaugeChart(3, "Kitchen - Humidity"));
+        hp.add(new HumidityGaugeChart(2, "Living Room - Humidity", true));
+        hp.add(new HumidityGaugeChart(1, "Bedroom 1 - Humidity", true));
+        hp.add(new HumidityGaugeChart(3, "Kitchen - Humidity", true));
+        hp.add(new HumidityGaugeChart(4, "Outside (NW) - Humidity", false));
+
 
 //        add(new LiveChart(2, "Living Room - Humidity"));
 //        add(new LiveChart(1, "Bedroom 1 - Humidity"));
 //        add(new LiveChart(3, "Kitchen - Humidity"));
+//        add(new LiveChart(4, "Outside (NW) - Humidity"));
 
 
     }
