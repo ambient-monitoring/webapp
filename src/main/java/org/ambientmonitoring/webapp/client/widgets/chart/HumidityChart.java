26564b8c99185215a71f2626ac4466b52f738ba1
Emil Burzo <eaglex@gmail.com>
2015-11-18 17:10:06 +0200

    add humidity

---
+++ src/main/java/org/ambientmonitoring/webapp/client/widgets/chart/HumidityChart.java
@@ -0,0 +1,139 @@
+package org.ambientmonitoring.webapp.client.widgets.chart;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.SimplePanel;
+import org.ambientmonitoring.webapp.client.rpc.AmbientRPC;
+import org.ambientmonitoring.webapp.shared.rpc.ReadingRPC;
+import org.moxieapps.gwt.highcharts.client.*;
+import org.moxieapps.gwt.highcharts.client.labels.DataLabels;
+import org.moxieapps.gwt.highcharts.client.plotOptions.BarPlotOptions;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HumidityChart extends SimplePanel {
+
+    private Map<Integer, Series> seriesMap = new HashMap<>();
+    private Chart chart;
+    private long lastTimestamp;
+
+    public HumidityChart() {
+        initUi();
+
+        // todo proper server side call
+        long now = new Date().getTime();
+        long fourHoursAgo = now - (4 * 3600 * 1000);
+//        long fourHoursAgo = now - (900 * 1000);
+
+        loadValues(fourHoursAgo, false);
+
+        loadNewValues();
+    }
+
+    private void initUi() {
+        chart = createChart();
+
+        setWidget(chart);
+    }
+
+    private Chart createChart() {
+        Chart chart = new Chart()
+                .setType(Series.Type.SPLINE)
+                .setMarginRight(10)
+                .setChartTitleText("Humidity")
+                .setBarPlotOptions(new BarPlotOptions().setDataLabels(new DataLabels().setEnabled(false)))
+                .setLegend(new Legend().setEnabled(true))
+                .setCredits(new Credits().setEnabled(false))
+                .setToolTip(new ToolTip().setEnabled(false))
+                ;
+
+        // X axis
+        chart.getXAxis().setType(Axis.Type.DATE_TIME);
+
+        // Y axis
+        chart.getYAxis()
+                .setAxisTitleText("Humidity %")
+                .setPlotLines(chart.getYAxis().createPlotLine()
+                                .setValue(0)
+                                .setWidth(1)
+                                .setColor("#808080")
+                );
+
+        // series
+        initSeries(chart);
+
+        return chart;
+    }
+
+    private void initSeries(Chart chart) {
+        // todo load from DB
+
+        // id 1 - bedroom 1
+        Series series = chart.createSeries().setName("Bedroom 1");
+        chart.addSeries(series);
+        seriesMap.put(1, series);
+
+        // id 2 - living room
+        series = chart.createSeries().setName("Living Room");
+        chart.addSeries(series);
+        seriesMap.put(2, series);
+
+        // id 3 - kitchen
+        series = chart.createSeries().setName("Kitchen");
+        chart.addSeries(series);
+        seriesMap.put(3, series);
+
+        // id 4 - outside
+        series = chart.createSeries().setName("Outside (NW)");
+        chart.addSeries(series);
+        seriesMap.put(4, series);
+    }
+
+    private void loadValues(long ts, final boolean animate) {
+        AmbientRPC.getReadingsSince(ts, new AsyncCallback<List<ReadingRPC>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(List<ReadingRPC> readings) {
+                loadFromReadings(readings, animate);
+            }
+        });
+    }
+
+    private void loadFromReadings(List<ReadingRPC> readings, boolean animate) {
+        for (ReadingRPC reading : readings) {
+            Series series = seriesMap.get(reading.id);
+
+            if (animate) {
+                series.addPoint(reading.timestamp, reading.humidity);
+            } else {
+                series.addPoint(reading.timestamp, reading.humidity, false, false, false);
+            }
+
+            lastTimestamp = reading.timestamp;
+        }
+
+        if (!animate) {
+            chart.redraw();
+        }
+    }
+
+    private void loadNewValues() {
+        Timer timer = new Timer() {
+            @Override
+            public void run() {
+                loadValues(lastTimestamp, true);
+            }
+        };
+
+        // todo some main thread that dispatches to all widgets
+        // todo start/stop on bind/unbind
+        timer.scheduleRepeating(30 * 1000);
+    }
+}
--- src/main/java/org/ambientmonitoring/webapp/client/widgets/graph/Graph.java
+++ src/main/java/org/ambientmonitoring/webapp/client/widgets/graph/Graph.java
@@ -5,8 +5,11 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
+import org.ambientmonitoring.webapp.client.widgets.chart.HumidityChart;
 import org.ambientmonitoring.webapp.client.widgets.chart.TemperatureChart;
+import org.gwtbootstrap3.client.ui.Column;
 import org.gwtbootstrap3.client.ui.Row;
+import org.gwtbootstrap3.client.ui.constants.ColumnSize;
 
 import java.util.List;
 
@@ -27,7 +30,14 @@ public class Graph extends SimplePanel {
     private void initUi() {
         setWidget(uiBinder.createAndBindUi(this));
 
-        fieldRow.add(new TemperatureChart());
+        Column column = new Column(ColumnSize.XS_12, ColumnSize.SM_12, ColumnSize.MD_6, ColumnSize.LG_6);
+        column.add(new TemperatureChart());
+        fieldRow.add(column);
+
+        column = new Column(ColumnSize.XS_12, ColumnSize.SM_12, ColumnSize.MD_6, ColumnSize.LG_6);
+        column.add(new HumidityChart());
+        fieldRow.add(column);
+
     }
 
 }
