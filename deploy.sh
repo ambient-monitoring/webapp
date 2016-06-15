0407dff12d864fc924bdba9812b0e51ba7670f31
Emil Burzo <eaglex@gmail.com>
2016-06-15 18:11:21 +0300

    update temporary deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ mvn package
 
 ssh root@ambient '/etc/init.d/jetty8 stop'
 
-scp /home/eaglex/devel/self/ambient-monitoring/webapp/target/AmbientMonitoring-1.0-SNAPSHOT.war root@ambient:/root/
-
-ssh root@ambient 'mv /root/AmbientMonitoring-1.0-SNAPSHOT.war /var/lib/jetty8/webapps/root.war'
+scp target/AmbientMonitoring-1.0-SNAPSHOT.war root@ambient:/var/lib/jetty8/webapps/root.war
 
 ssh root@ambient '/etc/init.d/jetty8 start'
+
