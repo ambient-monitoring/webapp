c8485f0eed82d6ea1e830b774a4215b93af3f06c
Emil Burzo <contact@emilburzo.com>
2017-02-21 19:13:21 +0200

    update local deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -eu
 
 mvn package
 
-ssh root@ambient '/etc/init.d/jetty8 stop'
+ssh root@ambient '/etc/init.d/jetty9 stop'
 
-scp target/AmbientMonitoring-1.0-SNAPSHOT.war root@ambient:/var/lib/jetty8/webapps/root.war
+scp target/AmbientMonitoring-1.0-SNAPSHOT.war root@ambient:/var/lib/jetty9/webapps/root.war
 
-ssh root@ambient '/etc/init.d/jetty8 start'
+ssh root@ambient '/etc/init.d/jetty9 start'
 
